935c1ec714556efb5639313375ca51a98fa2c9b9
justin_zhang@brown.edu <justinzhang@justins-mbp-5.devices.brown.edu>
2019-04-08 17:07:09 -0400

    demographic graphics workgit add -Agit add -Agit add -A

--- public/eatc/src/Content.js
+++ public/eatc/src/Content.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import img from './images/stat.png'; // Tell Webpack this JS file uses this image
+// import add_d3 from './demographics.js'
 
+import * as d3pie from "d3pie"
 
 export class MainContent extends Component {
   constructor(props){
@@ -13,7 +15,7 @@ export class MainContent extends Component {
       <div className="main-wrapper">
         <h>{this.props.data.name}</h> <br/>
         
-        {this.props.data.intro}      
+        {this.props.data.intro}   
       </div>
     );
   }
@@ -29,6 +31,126 @@ export class StatContent extends Component {
         <h>STATISTICS</h> <br/>
          <br/>
         <img src={img}/>
+      </div>
+    );
+  }
+}
+
+export class TestContent extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount() {
+    this.drawChart();
+  }
+
+  drawChart() {
+    console.log(d3pie)
+    var pie = new d3pie("pieChart", {
+  "header": {
+    "title": {
+      "text": "GENDER",
+      "fontSize": 16
+    },
+    "subtitle": {
+      "color": "#999999",
+      "fontSize": 10,
+      "font": "courier"
+    },
+    "location": "pie-center",
+    "titleSubtitlePadding": 10
+  },
+  "footer": {
+    "color": "#999999",
+    "fontSize": 10,
+    "font": "open sans",
+    "location": "bottom-left"
+  },
+  "size": {
+    "canvasHeight": 400,
+    "canvasWidth": 450,
+    "pieInnerRadius": "69%",
+    "pieOuterRadius": "48%"
+  },
+  "data": {
+    "sortOrder": "label-desc",
+    "content": [
+      {
+        "label": "male",
+        "value": 50,
+        "color": "#EB5757"
+      },
+      {
+        "label": "female",
+        "value": 44,
+        "color": "#2F80ED"
+      },
+      {
+        "label": "nonbinary/genderqueer",
+        "value": 4,
+        "color": "#D8269B"
+      },
+      {
+        "label": "agender",
+        "value": 2,
+        "color": "#9B51E0"
+      }
+    ]
+  },
+  "labels": {
+    "outer": {
+      "format": "label-value1",
+      "pieDistance": 20
+    },
+    "inner": {
+      "format": "none"
+    },
+    "mainLabel": {
+      "fontSize": 11
+    },
+    "percentage": {
+      "color": "#999999",
+      "fontSize": 11,
+      "decimalPlaces": 0
+    },
+    "value": {
+      "color": "#37859a",
+      "fontSize": 11
+    },
+    "lines": {
+      "enabled": true,
+      "style": "straight",
+      "color": "#1d1d1d"
+    },
+    "truncation": {
+      "enabled": true
+    }
+  },
+  "tooltips": {
+    "enabled": true,
+    "type": "placeholder",
+    "string": "{label}: {value}, {percentage}%",
+    "styles": {
+      "backgroundColor": "#363636",
+      "padding": 6
+    }
+  },
+  "effects": {
+    "pullOutSegmentOnClick": {
+      "effect": "back",
+      "speed": 400,
+      "size": 8
+    }
+  }
+});
+  }
+  render() {
+    return (
+      <div className="main-wrapper">
+        <h>STATISTICS</h> <br/>
+         <br/>
+        <div id="pieChart"></div>
       </div>
     );
   }
