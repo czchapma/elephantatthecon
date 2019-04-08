eb3a19e40f1ba53f8185730e1ede0d4bf0adee3b
Hannah Chow <hannah_chow@brown.edu>
2019-04-08 00:31:08 -0400

    sass finally works wow

--- public/eatc/src/App.js
+++ public/eatc/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 import {MainContent,StatContent,StoryContent} from './Content.js';
 
