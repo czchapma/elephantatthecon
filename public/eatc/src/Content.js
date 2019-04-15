2f89e5a20f6081175933c360ad1997da121e90df
justin_zhang@brown.edu <justinzhang@justins-mbp-5.devices.brown.edu>
2019-04-15 14:31:01 -0400

    fix bugs

--- public/eatc/src/Content.js
+++ public/eatc/src/Content.js
@@ -15,10 +15,7 @@ export class MainContent extends Component {
   render() {
     return (
       <div className="main-wrapper">
-        <h>MAGFEST</h> <br/>
-        {this.props.data}
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a leo eget ligula rhoncus finibus. Donec ornare elit sed sem fermentum, eget consectetur magna vulputate. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus at pellentesque diam. Cras placerat ex felis, feugiat rhoncus turpis interdum sit amet. Donec in arcu vitae massa imperdiet auctor. Aliquam venenatis iaculis metus. Nunc et faucibus libero, placerat fringilla velit. Nullam condimentum, lectus vitae vestibulum porttitor, neque sapien fringilla mauris, sed condimentum purus sem ut leo. Nunc tellus turpis, sagittis et tincidunt vitae, convallis quis neque. Aenean sed cursus nisi. Praesent at posuere urna. Nullam eget ultricies est.</p>
-
+        
         <h>{this.props.data.name}</h> <br/>
 
         {this.props.data.intro}
