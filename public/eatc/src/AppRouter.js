a1b8520313bb308238ec5996f9190f5106d2d877
Yunxin Deng <yunxindeng@Yunxins-MacBook-Pro.local>
2019-07-07 12:38:56 -0400

    linked survey to Google Form and fixed hidden links

---
+++ public/eatc/src/AppRouter.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {HomeContent} from './Content.js';
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            {/* <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li> */}
+          </ul>
+        </nav>
+
+        <Route path="/home" exact component={HomeContent} />
+        {/* <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} /> */}
+      </div>
+    </Router>
+  );
+}
+
+export default AppRouter;
--- public/eatc/src/survey.js
+++ public/eatc/src/survey.js
@@ -1,21 +1,73 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Redirect } from 'react-router';
+import { HomeContent} from './Content.js';
+import './AppRouter.js';
 
 export class SurveyContent extends Component{
   constructor(props){
     super(props);
     this.state = {
-        safety: 'none'
+        safety: 'none',
+        verbal:'none',
+        redirectToHome: false
     };
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
   onAddressChanged = (e) => {
+    const newState = e.currentTarget.getAttribute('value'); 
+    
     this.setState({
-      safety: e.currentTarget.getAttribute('value')
+      // safety: e.currentTarget.getAttribute('value')
+      text: newState,
     });
   }
 
+  handleChange = changeEvent => {
+    this.setState({
+      safety: changeEvent.target.value
+    });
+  };
+
+  handleChangeVerbal = changeEvent => {
+    this.setState({
+      verbal: changeEvent.target.value
+    });
+  };
+
+  handleChangeTouch = changeEvent => {
+    this.setState({
+      touch: changeEvent.target.value
+    });
+  };
+  handleChangeS = changeEvent => {
+    this.setState({
+      sexual: changeEvent.target.value
+    });
+  };
+
+  handleChangeD = changeEvent => {
+    this.setState({
+      describe: changeEvent.target.value
+    });
+  };
+
+  handleFormSubmit = formSubmitEvent => {
+    this.setState({
+      redirectToHome: true
+    });
+    alert(`Thank you!`);
+    //formSubmitEvent.preventDefault();
+  };
+
   render(){
+    const redirectToHome = this.state.redirectToHome;
+    if (redirectToHome === true) {
+        // return <Redirect to="/home" />
+        window.location.reload();
+    }
+
     return (
       <div id="home-content-wrapper">
 
@@ -25,81 +77,66 @@ export class SurveyContent extends Component{
             </div>
             <div id="survey-information">
               <p>VidCon is a multi-genre online video conference, held 
-                annually in Southern California since 2010.  By completing 
-                this survey, I acknowledge that my answers may be used by 
-                VidCon staff for future Uplift Work to promote safer 
-                conventions.
+                annually in Southern California since 2010.  The results of this survey will be shared in a future report and may be used by Uplift for future work to promote safe conventions and shared with VidCon staff.  By completing this survey, you acknowledge that that your responses will be used for these purposes.
               </p>
             </div>
           </div>
-
-          <form>
-            <div id="survey-form">
-
-              <div id="event-info">
-                <div className="survey-header">
+          
+          <iframe name="hidden_iframe" id="hidden_iframe"height="0px" width="0px" frameBorder="0px"></iframe>
+          <form id="myform" action="https://docs.google.com/forms/d/e/1FAIpQLSdDB3osiIabN3qamlMYG0_cdh3LKQGdGHumFe_FPmuH91QA3w/formResponse" method="post" target="hidden_iframe" onSubmit={this.handleFormSubmit}>
+          <div className="survey-header">
                   Event Information
                 </div>
+            
+            <div className = "survey-question">
+              <div className = "question-text"> I feel safe at VidCon:</div>
+              <div className="radio-row">
+                <label><input type="radio" name="entry.351585626" value="always"checked={this.state.safety === "always"}
+                onChange={this.handleChange}className="form-check-input"/>Always</label>
+                <label><input type="radio" name="entry.351585626" value="usually"checked={this.state.safety === "usually"}
+                onChange={this.handleChange}className="form-check-input"/>Usually</label>
+                <label><input type="radio" name="entry.351585626" value="sometimes"checked={this.state.safety === "sometimes"}
+                onChange={this.handleChange}className="form-check-input"/>Sometimes</label>
+                <label><input type="radio" name="entry.351585626" value="rarely"checked={this.state.safety === "rarely"}
+                onChange={this.handleChange}className="form-check-input"/>Rarely</label>                 
+              </div>
+            </div>
 
-                <div className = "survey-question">
-                  <div className = "question-text"> I felt safe:</div>
-                  <div className = "radio-row">
-                      <input type="radio" name = "safety" value="Always"/>
-                      <span value="Always" onClick={this.onAddressChanged}><div className="check" /></span>  Always
+            <div className = "survey-question">
+              <div className = "question-text">Someone has verbally harrassed me at VidCon:</div>
+              <div className="radio-row">
+              <label><input type="radio" name="entry.556186155" value="yes"checked={this.state.verbal === "yes"}
+              onChange={this.handleChangeVerbal}className="form-check-input"/>Yes</label>
+              <label><input type="radio" name="entry.556186155" value="no"checked={this.state.verbal === "no"}
+              onChange={this.handleChangeVerbal}className="form-check-input"/>No</label>             
+              </div>
+            </div>
+            
+            <div className = "survey-question">
+              <div className = "question-text">Someone has touched me without my consent at VidCon:</div>
+              <div className="radio-row">
+              <label><input type="radio" name="entry.1792789459" value="yes"checked={this.state.touch === "yes"}
+              onChange={this.handleChangeTouch}className="form-check-input"/>Yes</label>
+              <label><input type="radio" name="entry.1792789459" value="no"checked={this.state.touch === "no"}
+              onChange={this.handleChangeTouch}className="form-check-input"/>No</label>             
+              </div>
+            </div>
 
-                      <input type="radio" name = "safety" value="Usually"/>
-                      <span value="Usually" onClick={this.onAddressChanged}><div className="check"/></span>  Usually
+            <div className = "survey-question">
+              <div className = "question-text">Someone has sexually harrassed or assaulted me at VidCon:</div>
+              <div className="radio-row">
+              <label><input type="radio" name="entry.939669801" value="yes"checked={this.state.sexual === "yes"}
+              onChange={this.handleChangeS}className="form-check-input"/>Yes</label>
+              <label><input type="radio" name="entry.939669801" value="no"checked={this.state.sexual === "no"}
+              onChange={this.handleChangeS}className="form-check-input"/>No</label>             
+              </div>
+            </div>
 
-                      <input type="radio" name = "safety" value="Sometimes"/>
-                      <span value="Sometimes" onClick={this.onAddressChanged}><div className="check"/></span>  Sometimes
-
-                      <input type="radio" name = "safety" value="Rarely"/>
-                      <span value="Rarely" onClick={this.onAddressChanged}><div className="check"/></span>  Rarely
-
-                      <input type="radio" name = "safety" value="Never"/>
-                      <span value="Never" onClick={this.onAddressChanged}><div className="check"/></span>  Never
-                  </div>
-                </div>
-
-                <div className = "survey-question">
-                  <div className = "question-text"> Someone verbally harassed me:</div>
-                  <div className = "radio-row">
-                    <input type="radio" name = "safety" value="Always"/>
-                    <span value="Always" onClick={this.onAddressChanged}><div className="check" /></span>  Yes
-
-                    <input type="radio" name = "safety" value="Usually"/>
-                    <span value="Usually" onClick={this.onAddressChanged}><div className="check"/></span>  No
-                  </div>
-                </div>
-
-                <div className = "survey-question">
-                  <div className = "question-text"> Someone touched me without my consent:</div>
-                  <div className = "radio-row">
-                    <input type="radio" name = "safety" value="Always"/>
-                    <span value="Always" onClick={this.onAddressChanged}><div className="check" /></span>  Yes
-
-                    <input type="radio" name = "safety" value="Usually"/>
-                    <span value="Usually" onClick={this.onAddressChanged}><div className="check"/></span>  No
-                  </div>
-                </div>
-
-                <div className = "survey-question">
-                  <div className = "question-text"> Someone sexually harassed or assaulted me:</div>
-                  <div className = "radio-row">
-                    <input type="radio" name = "safety" value="Always"/>
-                    <span value="Always" onClick={this.onAddressChanged}><div className="check" /></span>  Yes
-
-                    <input type="radio" name = "safety" value="Usually"/>
-                    <span value="Usually" onClick={this.onAddressChanged}><div className="check"/></span>  No
-                  </div>
-                </div>
-                <br></br>
-                <div className = "survey-question surveyFill">
+            <div className = "survey-question surveyFill">
                   <div className = "question-text filltext"> At any point in the weekend, did you feel uncomfortable at MAGFest or see others in a difficult situation?</div>
                   <div className = "radio-row">
-                    <textarea value="" />
+                    <textarea value={this.state.value} onChange={this.onAddressChanged} name="entry.872631186"/>
                   </div>
-                </div>
             </div>
 
             <div id="demographic-info">
@@ -108,42 +145,40 @@ export class SurveyContent extends Component{
               The following demographic questions are optional but will help us analyze data by demographics to discover safety trends that may correlate with specific identities.
               </div>
               <div id="demo-left">
-                <div className = "question-text-demo"> What is your sexuality?</div>
-                <textarea value="" />
+                <div className = "question-text-demo"> What is your gender?</div>
+                <textarea value={this.state.value} onChange={this.onAddressChanged}name="entry.722266204"/>
+                
                 <div className = "question-text-demo"> Do you have a disability or chronic illness?</div>
-                <textarea value="" />
+                <textarea value={this.state.value} onChange={this.onAddressChanged} name="entry.2033834358"/>
+
                 <div className = "question-text-demo"> What is your race?</div>
-                <textarea value="" />
+                <textarea value={this.state.value} onChange={this.onAddressChanged} name="entry.630973531"/>
               </div>
               <div id="demo-right">
-                <div className = "question-text-demo"> What is your gender?</div>
-                <textarea id = "nice" value="" />
-                <div className = "question-text-demo"> Which of the following (if any) apply to you?</div>
+                <div className = "question-text-demo"> What is your sexuality?</div>
+                <textarea id = "nice" value={this.state.value} onChange={this.onAddressChanged} name="entry.1281966181"/>
+                <div className = "question-text-demo"> How would you describe yourself</div>
 
                 <div className = "multipleChoice-rows">
-                  <div className = "multOptions">
-                    <input type="radio" name = "safety" value="Always"/>
-                    <span class="leftradio" value="Always" onClick={this.onAddressChanged}><div className="check" /></span>  Cis
-                  </div>
 
-                  <div className = "multOptions">
-                  <input type="radio" name = "safety" value="Usually"/>
-                  <span class="leftradio" value="Usually" onClick={this.onAddressChanged}><div className="check"/></span>  Trans
-                  </div>
 
-                  <div className = "multOptions">
+                  <div className = "radio-row">
+                    <label><input type="radio" name="entry.130234048" value="cis"checked={this.state.describe === "cis"}
+                    onChange={this.handleChangeD}className="form-check-input"/>Cis</label>
 
-                  <input type="radio" name = "safety" value="Usually"/>
-                  <span class="leftradio" value="Usually" onClick={this.onAddressChanged}><div className="check"/></span>
-                  <textarea id = "ugh" value="" />
+                    <label><input type="radio" name="entry.130234048" value="trans"checked={this.state.describe === "trans"}
+                    onChange={this.handleChangeD}className="form-check-input"/>Trans</label>
+
+                    <label><input type="radio" name="entry.130234048.other_option_response" value="other"checked={this.state.describe === "other"}
+                    onChange={this.handleChangeD}className="form-check-input"/>Other:</label>
+                    <input type="text" name="entry.86141345" value={this.state.value} onChange={this.onAddressChanged}/>
+                    {/* <textarea value={this.state.value} onChange={this.onAddressChanged} name="entry.872631186"/>        */}
                   </div>
                 </div>
 
               </div>
             </div>
-
-          </div>
-          <div className = "buttonContainer">
+          <div className = "buttonContainer" id="submit_div">
             <input id = "submitButton" type="submit" value="Submit Form"/>
           </div>
 
